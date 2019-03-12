cfceb53b5f46ca05fa84af87ae616e5b7d485f19
a.pomosov <a.pomosov@alibaba-inc.com>
2019-03-13 01:45:18 +0300

    Fic controller to return json

--- src/main/kotlin/BillingController.kt
+++ src/main/kotlin/BillingController.kt
@@ -6,21 +6,23 @@ class BillingController(billingService: BillingService) {
         post("/account/create") { req, res ->
             res.type("application/json")
             val amount = (req.queryMap("amount").value() ?: "0").toLong()
-            if (amount < 0) return@post "Initial amount must not be negative".also { res.status(400) }
+            if (amount < 0) return@post "Initial amount must not be negative".toJsonError().also { res.status(400) }
             val account = billingService.createAccount(amount)
             Klaxon().toJsonString(account)
         }
         put("/transfer") { req, res ->
-            val from = req.queryMap()["from"].value() ?: return@put "Source account not specified".also { res.status(400) }
-            val to = req.queryMap()["to"].value() ?: return@put "Target account not specified".also { res.status(400) }
-            val amount = req.queryMap()["amount"].value()?.toLong() ?: return@put "Amount not specified".also { res.status(400) }
+            res.type("application/json")
+            val from =
+                req.queryMap()["from"].value() ?: return@put "Source account not specified".toJsonError().also { res.status(400) }
+            val to = req.queryMap()["to"].value() ?: return@put "Target account not specified".toJsonError().also { res.status(400) }
+            val amount = req.queryMap()["amount"].value()?.toLong() ?: return@put "Amount not specified".toJsonError().also { res.status(400) }
 
-            val fromAcc = billingService.accounts[from] ?: return@put "No such account $from".also { res.status(404) }
-            val toAcc = billingService.accounts[to] ?: return@put "No such account $to".also { res.status(404) }
+            val fromAcc = billingService.accounts[from] ?: return@put "No such account $from".toJsonError().also { res.status(404) }
+            val toAcc = billingService.accounts[to] ?: return@put "No such account $to".toJsonError().also { res.status(404) }
             try {
                 billingService.transfer(fromAcc, toAcc, amount)
             } catch (e: TransferException) {
-                return@put e.message.also { res.status(406) }
+                return@put e.message!!.toJsonError().also { res.status(406) }
             }
         }
         get("/account/list") { req, res ->
@@ -33,6 +35,8 @@ class BillingController(billingService: BillingService) {
         }
     }
 }
+
+fun String.toJsonError() = "{\"error\":\"$this\"}"
 
 fun main(args: Array<String>) {
     BillingController(BillingService()) //Keep it simple, no DI framework this time
